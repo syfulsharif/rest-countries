69414a4b6abfc45b76452b759a8dcc0b46cec442
syfulsharif <sharif.syful@gmail.com>
2021-09-18 13:49:49 +0600

    CSS added

--- js/countries.js
+++ js/countries.js
@@ -5,16 +5,19 @@ const loadCountries = ( ) => {
 }
 
 const displayCountries = countries => {
+    const container = document.getElementById('countries')
     console.log(countries[0]);
-    const countryHTML = countries.map(country => );
+    const countryHTML = countries.map(country => getCountryHTML(country));
+    container.innerHTML = countryHTML[0];
+    console.log(countryHTML[0]);
 }
 
 const getCountryHTML = country => {
     return `
-        <div>
+        <div class="country">
             <h2>${country.name}</h2>
             <h4>Capital : ${country.capital}</h4>
-            <img src="${country.flag}">
+            <img class="country-img" src="${country.flag}">
         </div>
     `;
 }
